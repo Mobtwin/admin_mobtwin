d2392438c369daf935ff3fe1481b36e13c7e345b
Bourichi-Taha <bourichi.taha@gmail.com>
2024-08-15 15:45:54 +0100

    error handler middleware

--- src/middlewares/errorHandler.middleware.ts
+++ src/middlewares/errorHandler.middleware.ts
@@ -5,6 +5,5 @@ import { logEvents } from './logger';
 // Centralized error handling middleware
 export const errorHandler = (err: Error, req: Request, res: Response, next: NextFunction) => {
   logEvents(`${err.name}: ${err.message}\t${req.method}\t${req.url}\t${req.headers.origin}`,'errLog.log');
-  console.error(err); // Log the error details for debugging
   sendErrorResponse(res, err, err?.message || 'Internal Server Error', 500);
 };

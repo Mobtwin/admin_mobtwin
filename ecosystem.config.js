331c51b3d35cf2c2fa0be7d12e402b43b20ae1ac
Bourichi-Taha <bourichi.taha@gmail.com>
2024-08-16 15:50:49 +0100

    ecosystem file

---
+++ ecosystem.config.js
@@ -0,0 +1,21 @@
+module.exports = {
+    apps: [
+      {
+        name: 'admin',        // Replace with your application's name
+        script: './dist/app.js',        // Replace with your main application file
+        instances: 1,            // Adjust according to your needs
+        autorestart: true,       // Enable automatic restart
+        watch: false,            // Set to true to auto-reload the app on file changes
+        max_memory_restart: '1G', // Restart the app if memory usage exceeds this value
+  
+        env: {
+          NODE_ENV: 'production', // Environment variables as needed
+          PORT: 3004              // Example environment variable
+        },
+  
+      }
+      // Add more apps as needed
+    ],
+  
+  };
+  

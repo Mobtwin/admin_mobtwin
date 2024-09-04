84443fe965e6a517cb9deaac5ef57c76914739e5
Bourichi-Taha <bourichi.taha@gmail.com>
2024-09-04 17:15:47 +0100

    secure always true for cookie

--- src/utils/jwt.ts
+++ src/utils/jwt.ts
@@ -61,7 +61,7 @@ export const createTokens = async(res: Response, payload: Express.User) => {
 
   res.cookie(environment.COOKIE_NAME, refreshToken, {
     httpOnly: true,
-    secure: process.env.NODE_ENV === 'production',
+    secure: true,
     sameSite: 'strict',
     maxAge: 1 * 24 * 60 * 60 * 1000, // 1 days
   });

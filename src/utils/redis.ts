31525d5afc5e4de44c36992a209be511e65e6ba4
Bourichi-Taha <bourichi.taha@gmail.com>
2024-09-12 15:46:43 +0100

    added parse int fron env to integer

--- src/utils/redis.ts
+++ src/utils/redis.ts
@@ -1,6 +1,6 @@
 import IOredis from 'ioredis';
 import { environment } from './loadEnvironment';
 environment.REDIS_PORT
-const redisClient = new IOredis(environment.REDIS_PORT || 6379, environment.REDIS_HOST || "172.28.1.7",{maxRetriesPerRequest:null});
+const redisClient = new IOredis(parseInt(environment.REDIS_PORT) || 6379, environment.REDIS_HOST || "172.28.1.7",{maxRetriesPerRequest:null});
 
 export default redisClient;

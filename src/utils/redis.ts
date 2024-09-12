6cb502d52c5621298bd38aff1674777f7e7ee10a
Bourichi-Taha <bourichi.taha@gmail.com>
2024-09-12 12:24:36 +0100

    redis ip from docker compose

--- src/utils/redis.ts
+++ src/utils/redis.ts
@@ -1,9 +1,10 @@
-import { Redis } from 'ioredis';
-import { environment } from './loadEnvironment';
+import { Redis } from "ioredis";
+import { environment } from "./loadEnvironment";
 
 const redisClient = new Redis({
-    maxRetriesPerRequest: null,
-    host: environment.REDIS_HOST || 'localhost',
-  });
+  host: environment.REDIS_HOST || "172.28.1.7",
+  port: 6379,
+  maxRetriesPerRequest: null,
+});
 
 export default redisClient;

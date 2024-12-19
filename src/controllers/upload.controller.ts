faf4506e82a3df9613019630b6dfadf826f1176e
Bourichi-Taha <bourichi.taha@gmail.com>
2024-12-19 13:47:05 +0100

    upload controller

---
+++ src/controllers/upload.controller.ts
@@ -0,0 +1,18 @@
+import { Request, Response } from "express";
+import { sendErrorResponse, sendSuccessResponse } from "../utils/response";
+import { uploadFilesToSpaces } from "../config/s3Client.config";
+
+export const uploadImageController = async (req: Request, res: Response) => {
+    const user = req.user;
+    if (!user) return sendErrorResponse(res,null, "Unauthorized!", 401);
+    const userId = user.id;
+    uploadFilesToSpaces(req, `admin/images`)
+      .then((imagesLink) => {
+        return sendSuccessResponse(res, { image: imagesLink[0] }, "Image uploaded successfully", 200);// [0] because upload single image
+      })
+      .catch((error) => {
+        return sendErrorResponse(res, error, "Error: " + error.message, 400);
+      });
+  };
+// userRouter.post('/change-avatar', imageUpload.single('avatar'), uploadAvatarController);
+//   

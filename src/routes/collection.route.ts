6f662bb39e109ee5ef8809c154e1362a57a69775
Bourichi-Taha <bourichi.taha@gmail.com>
2024-12-26 16:01:00 +0100

    collection routes create update

---
+++ src/routes/collection.route.ts
@@ -0,0 +1,230 @@
+import { Router } from "express";
+import { checkPermission } from "../middlewares/rbac.middleware";
+import { COLLECTION_PERMISSIONS } from "../constant/collection.constant";
+import { PERMISSION_TABLE } from "../constant/permission.constant";
+import { PERMISSIONS_ACTIONS } from "../constant/actions.constant";
+import { validateRequest } from "../middlewares/requestValidator.middleware";
+import { updatePermissionByIdSchema } from "../validators/permission.validator";
+import { createCollectionController, updateCollectionController } from "../controllers/collection.controller";
+import { createCollectionSchema } from "../validators/collection.validator";
+
+export const collectionRouter = Router();
+/**
+ * @swagger
+ * tags:
+ *   name: Collections
+ *   description: API to manage collections
+ */
+/**
+ * @swagger
+ * /collection:
+ *   post:
+ *     summary: Create a new collection
+ *     description: Allows authorized users to create a new collection. Requires the CREATE permission.
+ *     tags:
+ *       - Collections
+ *     security:
+ *       - bearerAuth: []
+ *     parameters:
+ *       - in: query
+ *         name: platform
+ *         schema:
+ *           type: string
+ *           example: "web"
+ *         description: Platform for which the collection is being created.
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             $ref: '#/components/schemas/CreateCollection'
+ *     responses:
+ *       201:
+ *         description: Collection created successfully.
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 success:
+ *                   type: boolean
+ *                   example: true
+ *                 message:
+ *                   type: string
+ *                   example: Collection created successfully!
+ *                 data:
+ *                   $ref: '#/components/schemas/Collection'
+ *       400:
+ *         description: Bad request due to validation errors or invalid query parameters.
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 success:
+ *                   type: boolean
+ *                   example: false
+ *                 error:
+ *                   type: string
+ *                   example: Invalid platform query parameter
+ *       401:
+ *         description: Unauthorized - Missing or invalid JWT token.
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 success:
+ *                   type: boolean
+ *                   example: false
+ *                 error:
+ *                   type: string
+ *                   example: Unauthorized!
+ *       403:
+ *         description: Forbidden. User lacks sufficient permissions
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 success:
+ *                   type: boolean
+ *                   example: false
+ *                 error:
+ *                   type: string
+ *                   example: Forbidden. Insufficient permissions!
+ *       500:
+ *         description: Internal server error.
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 success:
+ *                   type: boolean
+ *                   example: false
+ *                 error:
+ *                   type: string
+ *                   example: Error! Unable to create collection
+ */
+
+collectionRouter.post(
+  "/",
+  checkPermission([COLLECTION_PERMISSIONS.CREATE], {
+    table: PERMISSION_TABLE,
+    action: PERMISSIONS_ACTIONS.CREATE,
+  }),
+  validateRequest(createCollectionSchema),
+  createCollectionController
+);
+
+/**
+ * @swagger
+ * /collection/{id}:
+ *   put:
+ *     summary: Update an existing collection
+ *     description: Allows authorized users to update a collection by its ID. Requires the UPDATE permission.
+ *     tags:
+ *       - Collections
+ *     security:
+ *       - bearerAuth: []
+ *     parameters:
+ *       - in: path
+ *         name: id
+ *         required: true
+ *         schema:
+ *           type: string
+ *           example: "123e4567-e89b-12d3-a456-426614174000"
+ *         description: The ID of the collection to be updated.
+ *       - in: query
+ *         name: platform
+ *         schema:
+ *           type: string
+ *           example: "web"
+ *         description: Platform for which the collection is being updated.
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             $ref: '#/components/schemas/UpdateCollection'
+ *     responses:
+ *       200:
+ *         description: Collection updated successfully.
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 success:
+ *                   type: boolean
+ *                   example: true
+ *                 message:
+ *                   type: string
+ *                   example: Collection updated successfully!
+ *                 data:
+ *                   $ref: '#/components/schemas/Collection'
+ *       400:
+ *         description: Bad request due to validation errors or invalid input.
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 success:
+ *                   type: boolean
+ *                   example: false
+ *                 error:
+ *                   type: string
+ *                   example: Invalid platform query parameter
+ *       401:
+ *         description: Unauthorized - Missing or invalid JWT token.
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 success:
+ *                   type: boolean
+ *                   example: false
+ *                 error:
+ *                   type: string
+ *                   example: Unauthorized!
+ *       403:
+ *         description: Forbidden. User lacks sufficient permissions
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 success:
+ *                   type: boolean
+ *                   example: false
+ *                 error:
+ *                   type: string
+ *                   example: Forbidden. Insufficient permissions!
+ *       500:
+ *         description: Internal server error.
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 success:
+ *                   type: boolean
+ *                   example: false
+ *                 error:
+ *                   type: string
+ *                   example: Error! Unable to update collection
+ */
+
+
+collectionRouter.put(
+  "/:id",
+  checkPermission([COLLECTION_PERMISSIONS.UPDATE], {
+    table: PERMISSION_TABLE,
+    action: PERMISSIONS_ACTIONS.UPDATE,
+  }),
+  validateRequest(updatePermissionByIdSchema),
+  updateCollectionController
+);

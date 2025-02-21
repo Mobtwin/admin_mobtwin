331aae80d47acbaaeee02cbe11986171656d7a43
Bourichi-Taha <bourichi.taha@gmail.com>
2025-02-21 11:14:36 +0100

    disabling cors

--- src/app.ts
+++ src/app.ts
@@ -49,7 +49,7 @@ serverAdapter.setBasePath("/admin");
 app.use("/admin", serverAdapter.getRouter());
 
 // middleware to handle CORS
-app.use(cors(corsOptions));
+app.use(cors());
 app.use(cookieParser());
 // middleware to handle request logs
 app.use(logger);
